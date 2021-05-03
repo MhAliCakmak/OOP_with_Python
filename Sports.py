f25c5feeb08a05b908e364f607adf3800f9714df
Mehmet <202803055@ogr.cbu.edu.tr>
2021-05-03 12:43:01 +0300

    I use multi-inheritance in here

--- Sports.py
+++ Sports.py
@@ -1,22 +1,29 @@
 class footballer():
-    run="running"
-    sprint="sprinting"
-    price=5000
-    def __init__(self,foot="right"):
-        self.position="WF"
-        self.foot=foot
-class basketboller():
-    turnstile="turnstile"
-    trey="there-point"
-    price=7500
-    def __init__(self):
-         self.pos="pivot"
-class multisporcu(basketboller,footballer):
-    def __init__(self,foot):
-        basketboller.__init__(self)
-        footballer.__init__(self,foot)
+    run = "running"
+    sprint = "sprinting"
+    price = 5000
 
-mahmut=multisporcu("right")
+    def __init__(self, foot="right"):
+        self.position = "WF"
+        self.foot = foot
+
+
+class basketboller():
+    turnstile = "turnstile"
+    trey = "there-point"
+    price = 7500
+
+    def __init__(self):
+        self.pos = "pivot"
+
+
+class multisporcu(basketboller, footballer):
+    def __init__(self, foot):
+        basketboller.__init__(self)
+        footballer.__init__(self, foot)
+
+
+mahmut = multisporcu("right")
 print(mahmut.turnstile)
 
 print(mahmut.run)
